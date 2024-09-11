c20af0042060f3577cf313f78744034ef52f6b40
sudais <sudais@docme.cloud>
2024-09-11 19:00:55 +0530

    some features added in dragon

--- src/app/modules/user/pages/TigerDragon/tigergradon.component.ts
+++ src/app/modules/user/pages/TigerDragon/tigergradon.component.ts
@@ -10,12 +10,33 @@ export class DragonTiger{
 
     selectedAmount: number=50;
 
+    dragonAmt:number=0
+    tieAmt:number=0
+    tigerAmt:number=0
+
     /**slection of amount */
 
     selectAmount(value:any){
         this.selectedAmount = value;
         console.log(value);
         
+    }
+
+    /**dragon click */
+
+    dragonClick(amt:any){
+        const demomony =this.dragonAmt
+        this.dragonAmt = demomony+amt;
+    }
+
+    tieClick(amt:any){
+        const demomony =this.tieAmt
+        this.tieAmt = demomony+amt;
+    }
+
+    tigerClick(amt:any){
+        const demomony =this.tigerAmt
+        this.tigerAmt = demomony+amt;
     }
 
 }
